a469d98ee8a4a0a7ae608428ed7fa92951782951
hksoren <hksoren@windows>
2018-10-26 15:16:10 -0400

    added toomany test

--- test_rpn.py
+++ test_rpn.py
@@ -9,3 +9,9 @@ class TestBasics(unittest.TestCase):
 	def test_sub(self):
 		result = rpn.calculate('4 3 -')
 		self.assertEqual(1, result)
+
+	def test_toomany(self):
+		result = rpn.calculate('1 2 3 +')
+		self.assertRaises(ValueError)
+		with self.assertRaises(ValueError):
+			result = rpn.calculate('1 2 3 +')
