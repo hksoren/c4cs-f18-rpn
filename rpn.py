6697b0d4c5b5f5d9f3b461a56d67695ee8e48803
hksoren <hksoren@windows>
2018-10-26 15:23:03 -0400

    done!

--- rpn.py
+++ rpn.py
@@ -11,13 +11,23 @@ def calculate(arg):
     	except ValueError:
     		val1 = stack.pop()
     		val2 = stack.pop()
-    		result = val1 + val2
+    		if token == '+':
+    			result = val1 + val2
+    		elif token == '-':
+    			result = val2 - val1
     		stack.append(result)
+
+
+    if len(stack) > 1:
+    	raise ValueError("Too many arguments on the stack")		
     return stack[0]
 
 def main():
     while True:
-        result = calculate(input("rpn calc> "))
-        print(result)
+    	try:
+        	result = calculate(input("rpn calc> "))
+        	print(result)
+        except ValueError:
+        	pass
 if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
     main()
